32314c124cf14e7c21dfbabce6bf4eee08f2a792
Aristotle <kayafasluke@gmail.com>
2025-09-14 17:29:08 -0400

    inital commit

---
+++ create_description.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+import openpyxl
+import datetime as dt
+
+# Open the file as a pandas dataframe
+df = pd.read_excel("At Thrift.xlsx")
+
+date = dt.datetime.now().strftime("%d-%b-%Y")
+
+new_file = open(f"{date}_descriptions.txt", "w")
+
+for index, row in df.iterrows():
+    title = f"{row['Brand']} {row['Item']} {row['Size']}" 
+
+    cond = row['Condition']
+    size = row['Size']
+    mat = row['Fabric']
+    care = row['Washability']
+    ori = row['Country']
+    feat = row['Features']
+
+    desc = f"Title: {title}\nCondition: {cond}\nSize: {size}\nMaterial: {mat}\nCare Instructions: {care}\nMade In: {ori}\nFeatures: {feat}\nPlease comment with any questions! Measurements shown in photos.\nKeywords: \n"
+
+    new_file.write(f"{desc}\n")
+
+
